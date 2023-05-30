292e8d77d3776a0ce8fdf2e02699cd97a41336bd
youngil <youngil@placeb.com>
2023-05-30 09:53:26 +0900

    싱글턴

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -1,8 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Managers
+public class Managers : MonoBehaviour
 {
-    
+    static Managers s_instance;
+    public static Managers Instance { get { Init(); return s_instance; } }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+
+    }
 }
