e7dd3b532e27bbdf770f47d86b9b3e77e2b8a893
youngil <youngil@placeb.com>
2023-06-06 16:02:19 +0900

    Raycast #1

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -6,11 +6,25 @@ public class TestCollision : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision");
+        Debug.Log($"Collision @ {collision.gameObject.name}");
     }
 
     private void OnTriggerEnter(Collider other)
+    {        
+        Debug.Log($"Trigger @ {other.gameObject.name}");
+    }
+
+    private void Update()
     {
-        Debug.Log("Trigger");
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+
+        RaycastHit[] hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+
+        foreach (var hit in hits)
+        {
+            Debug.Log($"Raycast {hit.collider.gameObject.name}");
+        }
     }
 }
