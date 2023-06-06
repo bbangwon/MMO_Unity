4c4e869f9a5c9bd0ca14ffd7b280af56b624f9d4
youngil <youngil@placeb.com>
2023-06-06 16:39:38 +0900

    Raycasting #2

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -16,15 +16,48 @@ public class TestCollision : MonoBehaviour
 
     private void Update()
     {
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        //Vector3 look = transform.TransformDirection(Vector3.forward);
+        //Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
 
 
-        RaycastHit[] hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        //RaycastHit[] hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
 
-        foreach (var hit in hits)
+        //foreach (var hit in hits)
+        //{
+        //    Debug.Log($"Raycast {hit.collider.gameObject.name}");
+        //}
+
+        //스크린 좌표계 Input.mousePosition
+        //y+
+        //|
+        //*-- x+
+        //Debug.Log(Input.mousePosition);
+
+        //Viewport 좌표계  Screen좌표계의 0.0 ~ 1.0
+        //Debug.Log(Camera.main.ScreenToViewportPoint( Input.mousePosition ));
+
+        //Screen To WorldPosition
+        if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log($"Raycast {hit.collider.gameObject.name}");
+            //Camera Ray를 바로 가져오는 방법
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 1.0f);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f))
+            {
+                Debug.Log($"Raycast Camera {hit.collider.gameObject.name}");
+            }
+
+            //Ray를 직접 구하는 방법
+            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            //Vector3 dir = mousePos - Camera.main.transform.position;
+            //dir = dir.normalized;
+
+            //Debug.DrawRay(Camera.main.transform.position, dir * 100f, Color.red, 1.0f);
+            //if (Physics.Raycast(Camera.main.transform.position, dir, out RaycastHit hit, 100f))
+            //{
+            //    Debug.Log($"Raycast Camera {hit.collider.gameObject.name}");
+            //}
         }
     }
 }
