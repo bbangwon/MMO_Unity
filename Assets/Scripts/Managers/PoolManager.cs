7e16d2e57c5b9fa99b1b9a50348b41987ae1eaee
youngil <youngil@placeb.com>
2023-06-24 13:39:56 +0900

    PoolManager #2

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -1,15 +1,63 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class PoolManager
 {
+    #region Pool
     class Pool
     {
         public GameObject Original { get; private set; }
         public Transform Root { get; set; }
 
         Stack<Poolable> _poolStack = new Stack<Poolable>();
-    }
+
+        public void Init(GameObject original, int count = 5)
+        {
+            Original = original;
+            Root = new GameObject().transform;
+            Root.name = $"{original.name}_Root";
+
+            for (int i = 0; i < count; i++)
+                Push(Create());
+        }
+
+        Poolable Create()
+        {
+            GameObject go = Object.Instantiate(Original);
+            go.name = Original.name;
+            return go.GetOrAddComponent<Poolable>();
+        }
+
+        public void Push(Poolable poolable)
+        {
+            if (poolable == null)
+                return;
+
+            poolable.transform.parent = Root;
+            poolable.gameObject.SetActive(false);
+            poolable.IsUsing = false;
+
+            _poolStack.Push(poolable);
+        }
+
+        public Poolable Pop(Transform parent)
+        {
+            Poolable poolable;
+
+            if (_poolStack.Count > 0)
+                poolable = _poolStack.Pop();
+            else
+                poolable = Create();
+
+            poolable.gameObject.SetActive(true);
+            poolable.transform.parent = parent;
+            poolable.IsUsing = true;
+
+            return poolable;
+        }
+    } 
+    #endregion
 
     Dictionary<string, Pool> _pool = new Dictionary<string, Pool>();
     Transform _root;
@@ -23,18 +71,49 @@ public class PoolManager
         }
     }
 
+    public void CreatePool(GameObject original, int count = 5)
+    {
+        Pool pool = new Pool();
+        pool.Init(original, count);
+        pool.Root.parent = _root;
+
+        _pool.Add(original.name, pool);
+    }
+
     public void Push(Poolable poolable)
     {
+        string name = poolable.gameObject.name;
 
+        if(_pool.ContainsKey(name) == false)
+        {
+            Object.Destroy(poolable.gameObject);
+            return;
+        }
+
+        _pool[name].Push(poolable);
     }
 
     public Poolable Pop(GameObject original, Transform parent = null)
     {
-        return null;
+        if (_pool.ContainsKey(original.name) == false)
+            CreatePool(original);
+
+        return _pool[original.name].Pop(parent);
     }
 
     public GameObject GetOriginal(string name)
     {
-        return null;
+        if (_pool.ContainsKey(name) == false)
+            return null;
+
+        return _pool[name].Original;
+    }
+
+    public void Clear()
+    {
+        foreach (Transform child in _root)
+            Object.Destroy(child.gameObject);
+
+        _pool.Clear();
     }
 }
